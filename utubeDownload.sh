17678430b8fe0f8a9e20619309339fa36f6bbff0
Ashish Choithani <mr.akc@outlook.com>
2017-12-10 22:35:45 +0530

    Update uTubeDownloader.sh
    added quality support

--- utubeDownload.sh
+++ utubeDownload.sh
@@ -3,21 +3,39 @@
 
 declare -a link;
 read input;
+
+# link[1] -> url
+# link[2] -> a or v
+# link[3] -> quality
 i=0;
-
-
 for word in $input
 do
     link[$i]="$word";
     i=$((i+1));
 done
 
-if [[ ${link[1]} = "a" ]]; then
-    cd $HOME/Music;
-    youtube-dl -f m4a ${link[0]};
-else
-    cd $HOME/Videos;
-    youtube-dl ${link[0]};
+
+#quality
+#720 -> 22
+#1800 -> 37
+#default 480 -> 37 18maybe
+quality=18;
+
+if [[ ${link[2]} = "720" ]]; then
+    quality=22
+fi
+if [[ ${link[2]} = "1800" ]]; then
+    quality=37
 fi
 
-exit;         
+
+if [[ ${link[1]} = "a" ]]; then
+    cd $HOME/Music;
+    #youtube-dl -f m4a ${link[0]};
+    youtube-dl --extract-audio --audio-format mp3 ${link[0]};
+
+else
+    cd $HOME/Videos/YoutubeDl;
+    youtube-dl -f $quality ${link[0]};
+fi
+exit;
